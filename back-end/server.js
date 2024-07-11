345cc496937fedb7d94292227a06896751ac10d1
Ekaterina Bochvaroska <119041374+EkaterinaB29@users.noreply.github.com>
2024-07-11 10:30:02 +0200

    Establishing a connection to the database

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,19 @@
-require('dotenv').config()
-
 const express = require('express')
-const port = process.env.PORT || 5000
+const path = require('path')
+require('dotenv').config()
+//
+// Define the port
+const port = 8211;
+
+const connectDB = require('./config/dbConfig')
+
+connectDB()
+
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('hello'))
-
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, (err) => {
+  if (err) console.log(err)
+  console.log(`Server started on port ${port}`)
+})
